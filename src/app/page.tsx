1c756acb909e0d5ffa495137ac9f224a33e40277
Adria Martinez <adriamartinez94@gmail.com>
2024-04-08 21:31:18 +0200

    Add MessageInput component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import MessageInput from "@/app/chat/components/message-input";
+
 export default function Home() {
   return (
-    <main>
-      <p>Notego</p>
+    <main className=" h-screen w-screen inset-0 flex gap-4  ">
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1" />
+        <MessageInput />
+      </div>
     </main>
   );
 }
